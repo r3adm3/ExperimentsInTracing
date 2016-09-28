55063696845e173470ace560c813515947b4317f
Adrian Farnell <adrianjfarnell@gmail.com>
2016-09-28 10:40:59 +0100

    Add project files.

---
+++ ExperimentsInTracing/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExperimentsInTracing.MvcApplication" Language="C#" %>
---
+++ ExperimentsInTracing/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExperimentsInTracing.Startup))]
+namespace ExperimentsInTracing
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
